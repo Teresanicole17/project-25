3ba5aba0fed31e25ae75664b53fd4b808079c725
Teresanicole17 <teresa-judge@student.kirkwood.edu>
2026-02-04 09:26:13 -0600

    Add files via upload

---
+++ project25.ino
@@ -0,0 +1,29 @@
+// Project 25 – Using Interrupts
+
+#define LED 13
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  attachInterrupt(0, displayMicros, RISING);
+  attachInterrupt(1, displayMillis, RISING);  
+}
+1 void displayMicros()
+{
+  Serial.write("micros() = ");
+  Serial.println(micros());
+}
+
+void displayMillis()
+{
+  Serial.write("millis() = ");
+  Serial.println(millis());
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+}
